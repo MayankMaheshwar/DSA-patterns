4388e18de49f8b288cb4ed775952eaa89d5c0b20
Mayank Maheshwari <mayank.maheshwari625@gmail.com>
2024-09-08 15:24:38 +0530

    add s of solid

--- SOLID/single_responsibility.py
+++ SOLID/single_responsibility.py
@@ -1,5 +1,6 @@
 class Marker:
-    def __init__(self, color, thickness):
+    def __init__(self, color, thickness, price):
+        self.price = price
         self.color = color
         self.thickness = thickness
 
@@ -8,3 +9,36 @@ class Marker:
 
 
 class Invoice:
+
+    def __init__(self, marker, quantity):
+        self.quantity = quantity
+        self.marker = marker
+
+    def total(self):
+        return self.quantity * self.price
+    
+    " 2nd reason to change the class "
+    def printInvoice(self):
+        pass
+
+    " 3rd reason to change the class "
+    def saveToDatabase(self):
+        pass
+
+
+class invoicePrinter:
+
+    def __init__(self, invoice):
+        self.invoice = invoice
+
+    def printInvoice(self):
+        pass
+
+
+class invoiceDatabase:
+
+    def __init__(self, invoice):
+        self.invoice = invoice
+
+    def saveToDatabase(self):
+        pass
